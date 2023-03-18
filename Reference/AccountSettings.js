be55e5f1f5088e7e01198de020759d7bc7035b13
Riya Singh <82483823+EPro30@users.noreply.github.com>
2023-03-17 23:05:07 -0400

    AccountSettings Code snippet from code.js
    Removed AccountSettings function from code.js (line 749 to 758) because we are not using Account Settings anymore.
    So, just for reference the code snippet is available here.

---
+++ Reference/AccountSettings.js
@@ -0,0 +1,10 @@
+function AccountSettings() {
+    fName = document.getElementById("currentFName");
+    fName.innerText = firstName;
+
+    lName = document.getElementById("currentLName");
+    lName.innerText = lastName;
+
+    email = document.getElementById("currentEmail");
+    email.innerText = userId;
+}
